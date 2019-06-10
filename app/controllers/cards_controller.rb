8d11ea6fa430316417b9f0309c1c2358a3618f8a
Ykum4 <usedtoliveincanada@gamil.com>
2019-06-10 13:20:33 +0900

    create view of card-detail

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,22 @@
 class CardsController < ApplicationController
   before_action :get_payjp_customer
+  before_action :get_card_info
 
+  # カード登録していなければ追加するページへの遷移リンク
+  def addcard
+    if @card
+      render :show
+    else
+      render :new
+    end
+  end
+
+  # クレジットカード登録ページ
   def new 
+  end
+
+  def show
+    
   end
 
   def create
@@ -15,5 +30,10 @@ class CardsController < ApplicationController
   private
   def get_payjp_customer
     @payjp_customer =  Payjp::Customer.retrieve(current_user.payjp_customer_id)
+  end
+  def get_card_info
+    if @payjp_customer.default_card
+      return @card = @payjp_customer.cards.retrieve(@payjp_customer.default_card)
+    end
   end
 end
