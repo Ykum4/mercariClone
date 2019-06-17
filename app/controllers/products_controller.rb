66855df056a8e1300d724b4cb3fccd3218acc619
Ykum4 <usedtoliveincanada@gamil.com>
2019-06-17 10:46:29 +0900

    create view of product detail

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,28 @@
 class ProductsController < ApplicationController
 
+  # トップページ
   def index
-    
+    # 後にリファクタリングしていく
+    # 現状はladiesとmend、そしてシャネル/ルイヴィトンのみを表示させる
+    @ladies = Category.find_by(id: 1)
+    @mens = Category.find_by(id: 2)
+    @chanel = Brand.find_by(id: 1)
+    @vuitton = Brand.find_by(id: 2)
+
+    # includesで実験
+    # @ladies_product = Product.where(category_id: @ladies.id).limit(4).order(id: "DESC") これだとmain, subのカテゴリーを検索できない
+    @ladies_item = Product.joins(:category).merge(Category.where(main_category_id: 1)).limit(4).order(id: "DESC")
+
   end
 
+  # 商品出品画面
   def new
     @product = Product.new
+  end
+
+  # 商品詳細画面
+  def show
+    # @product = Product.find(params[:id])
   end
 
   def create 
